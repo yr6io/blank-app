8a0a560ce418d16ac10d50e6c61841b30239064b
yr6io <d2523@e-mirim.hs.kr>
2025-08-20 00:02:40 +0000

    01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🏫 우리반 알림장")
+st.write("우리 반의 중요한 소식을 확인하세요!")
